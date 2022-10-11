b09f01867a4617ac8ba2a2908a92e524a7cae303
TiagoJacintoDev <tiagojacintodev@gmail.com>
2022-10-11 11:15:10 +0100

    Configure React Router DOM

--- src/App.js
+++ src/App.js
@@ -1,1 +1,27 @@
-export default function App() {}
+import { useQuery } from '@tanstack/react-query';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Category from './pages/Category';
+import Home from './pages/Home';
+import { api } from './services/api';
+
+export default function App() {
+  const {
+    data: categories,
+    isLoading,
+    error,
+  } = useQuery(['categories'], async () => {
+    const response = await api.get('categories');
+    return response.data;
+  });
+
+  return (
+    <>
+      <Header categories={categories} />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/categories/:category' element={<Category />} />
+      </Routes>
+    </>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import App from './App';
 import './index.css';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './services/queryClient';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>
 );
