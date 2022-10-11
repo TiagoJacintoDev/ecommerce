d599403d07c03eaaf863c13569b49e0eed8bccd1
TiagoJacintoDev <tiagojacintodev@gmail.com>
2022-10-11 15:59:17 +0100

    Tweak Categories Menu to Work With Context

--- src/components/CategoriesMenu.jsx
+++ src/components/CategoriesMenu.jsx
@@ -1,7 +1,11 @@
 import { AiOutlineClose } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { EcommerceContext } from '../App';
+import { LowerFirst } from '../helpers/functions';
 
-export default function CategoriesMenu({ categories, closeMenu }) {
+export default function CategoriesMenu({ closeMenu }) {
+  const [, categories] = useContext(EcommerceContext);
   return (
     <div className='absolute w-64 top-0 left-0 bg-dark-blue text-white p-8 rounded-lg'>
       <div className='flex justify-between items-center mb-2 font-semibold'>
@@ -11,13 +15,10 @@ export default function CategoriesMenu({ categories, closeMenu }) {
         </button>
       </div>
       <ul className='flex flex-col items-center'>
-        {categories.map(category => (
-          <li className='my-2' key={category.id}>
-            <Link
-              onClick={closeMenu}
-              to={`/categories/${category.name.toLowerCase()}`}
-            >
-              <button className='hover:underline'>{category.name}</button>
+        {categories.data.map(category => (
+          <li className='my-2' key={category}>
+            <Link onClick={closeMenu} to={`/categories/${category.toLowerCase()}`}>
+              <button className='hover:underline'>{LowerFirst(category)}</button>
             </Link>
           </li>
         ))}
