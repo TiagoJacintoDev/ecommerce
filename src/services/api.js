c72c1621a3c71444fb19e9c9df74190219b2495b
TiagoJacintoDev <tiagojacintodev@gmail.com>
2022-10-11 15:56:39 +0100

    Change API

--- src/services/api.js
+++ src/services/api.js
@@ -1,3 +1,3 @@
 import axios from 'axios';
 
-export const api = axios.create({ baseURL: 'https://api.escuelajs.co/api/v1/' });
+export const api = axios.create({ baseURL: 'https://fakestoreapi.com/' });
