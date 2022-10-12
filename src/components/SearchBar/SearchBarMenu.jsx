8506bb5e90ed46fe767aa82ae14aa2e9082785c0
TiagoJacintoDev <tiagojacintodev@gmail.com>
2022-10-12 12:14:38 +0100

    Change Search Bar Menu Container Style

--- src/components/SearchBar/SearchBarMenu.jsx
+++ src/components/SearchBar/SearchBarMenu.jsx
@@ -8,12 +8,14 @@ export default function SearchBarMenu({ closeSearchMenu, history, search }) {
         className='fixed bg-[rgba(0,0,0,0.5)] left-0 w-[100vw] h-[300vw] -z-10'
         onClick={closeSearchMenu}
       />
-      <div className='container left-1/2 -translate-x-1/2 absolute top-full p-5 z-10 bg-white rounded-b-xl border border-accent'>
-        {search ? (
-          <SearchingMenu search={search} />
-        ) : (
-          <InitialMenu history={history} />
-        )}
+      <div className='search-bar-container left-1/2 -translate-x-1/2 absolute top-full p-5 z-10 bg-white rounded-b-xl border border-accent'>
+        <div className='grid grid-flow-col auto-cols-fr gap-10'>
+          {search ? (
+            <SearchingMenu search={search} />
+          ) : (
+            <InitialMenu history={history} />
+          )}
+        </div>
       </div>
     </>
   );
