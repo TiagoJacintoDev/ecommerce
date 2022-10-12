f28d18f2918715fa4decd852a3ea2c3506592dc6
TiagoJacintoDev <tiagojacintodev@gmail.com>
2022-10-12 12:16:09 +0100

    Search Bar Menu Grid Is A Parent

--- src/components/SearchBar/MenuContent/InitialMenu.jsx
+++ src/components/SearchBar/MenuContent/InitialMenu.jsx
@@ -1,6 +1,6 @@
 import { AiOutlineSearch } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
-import { useContext, useMemo, useState } from 'react';
+import { useContext, useMemo } from 'react';
 import { EcommerceContext } from '../../../App';
 import { getRandomProducts } from '../../../helpers/functions';
 
@@ -15,58 +15,53 @@ export default function InitialMenu({ history }) {
   const cloneProducts = [...products.data];
   const fiveRandomProducts = useMemo(() => getRandomProducts(products, 5), []);
   const tenHighestRatedProducts = useMemo(
-    () =>
-      cloneProducts
-        .sort((a, b) => b.rating.rate - a.rating.rate)
-        .filter((p, index) => index < 10),
+    () => cloneProducts.sort((a, b) => b.rating.rate - a.rating.rate).slice(0, 10),
     []
   );
 
   return (
     <>
-      <div className='font-semibold pb-3'>HIGHEST RATED PRODUCTS</div>
-      <div className='grid grid-flow-col auto-cols-fr gap-10'>
+      <div>
+        <div className='font-semibold pb-3'>HIGHEST RATED PRODUCTS</div>
+        {tenHighestRatedProducts.map(product => (
+          <Link
+            to={`/products/${product.title}`}
+            key={product.id}
+            className='search-link'
+          >
+            <AiOutlineSearch style={{ minHeight: '20px', minWidth: '20px' }} />
+            <h1>{product.title}</h1>
+          </Link>
+        ))}
         <div>
-          {tenHighestRatedProducts.map(product => (
-            <Link
-              to={`/products/${product.title}`}
-              key={product.id}
-              className='search-link'
-            >
-              <AiOutlineSearch style={{ minHeight: '20px', minWidth: '20px' }} />
-              <h1>{product.title}</h1>
-            </Link>
-          ))}
-          <div>
-            <div className='flex justify-between items-center mt-2'>
-              <div className='font-semibold'>SEARCH HISTORY</div>
-              <button className='underline'>Clean History</button>
-            </div>
-            <ul>
-              {firstFiveHistoryItems.map((item, index) => (
-                <li key={index}>{item}</li>
-              ))}
-            </ul>
+          <div className='flex justify-between items-center mt-2'>
+            <div className='font-semibold'>SEARCH HISTORY</div>
+            <button className='underline'>Clean History</button>
           </div>
+          <ul>
+            {firstFiveHistoryItems.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
         </div>
-        <div className='[&>*:not(:last-child)]:mb-10'>
-          {fiveRandomProducts.map(product => (
-            <div key={product.id} className='flex gap-6 items-center'>
-              <Link to={`/products/${product.title}`}>
-                <img className='max-h-20' src={product.image} />
+      </div>
+      <div className='[&>*:not(:last-child)]:mb-10'>
+        {fiveRandomProducts.map(product => (
+          <div key={product.id} className='flex gap-6 items-center'>
+            <Link to={`/products/${product.title}`}>
+              <img className='max-h-20' src={product.image} />
+            </Link>
+            <div className='flex flex-col'>
+              <Link
+                to={`/products/${product.title}`}
+                className='hover:underline hover:text-blue-700'
+              >
+                {product.title}
               </Link>
-              <div className='flex flex-col'>
-                <Link
-                  to={`/products/${product.title}`}
-                  className='hover:underline hover:text-blue-700'
-                >
-                  {product.title}
-                </Link>
-                <span className='font-bold'>${product.price}</span>
-              </div>
+              <span className='font-bold'>${product.price}</span>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
     </>
   );
