65379f509e24a711c05f2037da093fafc00329ad
TiagoJacintoDev <tiagojacintodev@gmail.com>
2022-10-12 12:15:08 +0100

    Implement No Results Found On Search Bar Menu

---
+++ src/components/SearchBar/MenuContent/NoResultsFound.jsx
@@ -0,0 +1,24 @@
+import { AiOutlineSearch } from 'react-icons/ai';
+import NotFoundImage from '../../../assets/1127749553-vector.jpg';
+
+export default function NoResultsFound({ search }) {
+  return (
+    <>
+      <div className='flex flex-col justify-center items-center'>
+        <div className='flex gap-3 items-center text-xl mb-10'>
+          <AiOutlineSearch />
+          <div>
+            There are no results for the search{' '}
+            <span className='font-bold m-0 p-0'>"{search}"</span>
+          </div>
+        </div>
+        <ul className='list-disc'>
+          <li>Check the spelling of inserted words</li>
+          <li>Enter a new keyword</li>
+          <li>Use synonyms or related searches</li>
+        </ul>
+      </div>
+      <img src={NotFoundImage} />
+    </>
+  );
+}
--- src/components/SearchBar/MenuContent/SearchingMenu.jsx
+++ src/components/SearchBar/MenuContent/SearchingMenu.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { EcommerceContext } from '../../../App';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { LowerFirst } from '../../../helpers/functions';
+import NoResultsFound from './NoResultsFound';
 
 export default function SearchingMenu({ search }) {
   const [products] = useContext(EcommerceContext);
@@ -39,53 +40,59 @@ export default function SearchingMenu({ search }) {
     [search]
   );
 
+  if (
+    products.data.filter(product =>
+      product.title.toLowerCase().includes(search.toLowerCase())
+    ).length === 0
+  ) {
+    return <NoResultsFound search={search} />;
+  }
+
   return (
     <>
-      <div className='font-semibold'>SEARCH SUGGESTIONS</div>
-      <div className='grid grid-flow-col auto-cols-fr gap-10'>
-        <div>
-          <div className='font-bold'>"{search}"</div>
+      <div>
+        <div className='font-semibold'>SEARCH SUGGESTIONS</div>
+        <div className='font-bold'>"{search}"</div>
+        <Link
+          to={`/category/${mostCommonCategory}`}
+          className='search-link ml-4 hover:font-semibold'
+        >
+          In {LowerFirst(mostCommonCategory)}
+        </Link>
+        {highestRatedProducts.map(product => (
           <Link
-            to={`/category/${mostCommonCategory}`}
-            className='search-link ml-4 hover:font-semibold'
+            to={`/products/${product.title}`}
+            key={product.id}
+            className='search-link'
           >
-            In {LowerFirst(mostCommonCategory)}
+            <AiOutlineSearch style={{ minHeight: '20px', minWidth: '20px' }} />
+            <h1>{product.title}</h1>
           </Link>
-          {highestRatedProducts.map(product => (
-            <Link
-              to={`/products/${product.title}`}
-              key={product.id}
-              className='search-link'
-            >
-              <AiOutlineSearch style={{ minHeight: '20px', minWidth: '20px' }} />
-              <h1>{product.title}</h1>
+        ))}
+        <Link
+          className='search-link text-gray-600 font-semibold hover:text-gray-600'
+          to={`/search/${search}`}
+        >
+          See all results for "{search}"
+        </Link>
+      </div>
+      <div className='[&>*:not(:last-child)]:mb-10'>
+        {filteredProducts.map(product => (
+          <div key={product.id} className='flex gap-6 items-center'>
+            <Link to={`/products/${product.title}`}>
+              <img src={product.image} className='max-h-20' />
             </Link>
-          ))}
-          <Link
-            className='search-link text-gray-600 font-semibold hover:text-gray-600'
-            to={`/search/${search}`}
-          >
-            See all results for "{search}"
-          </Link>
-        </div>
-        <div className='[&>*:not(:last-child)]:mb-10'>
-          {filteredProducts.map(product => (
-            <div key={product.id} className='flex gap-6 items-center'>
-              <Link to={`/products/${product.title}`}>
-                <img src={product.image} className='max-h-20' />
+            <div className='flex flex-col'>
+              <Link
+                to={`/products/${product.title}`}
+                className='hover:underline hover:text-blue-700'
+              >
+                {product.title}
               </Link>
-              <div className='flex flex-col'>
-                <Link
-                  to={`/products/${product.title}`}
-                  className='hover:underline hover:text-blue-700'
-                >
-                  {product.title}
-                </Link>
-                <span className='font-bold'>${product.price}</span>
-              </div>
+              <span className='font-bold'>${product.price}</span>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
     </>
   );
