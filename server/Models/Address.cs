0ce064d7fb1bc72a3a5de613f765ddab47badbb5
TiagoJacintoDev <tiagojacintodev@gmail.com>
2022-12-13 09:12:08 +0000

    Move Files & Configure Server

---
+++ server/Models/Address.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace server.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public int PhoneNumber { get; set; }
+        [JsonIgnore]
+        public User User { get; set; }
+    }
+}
